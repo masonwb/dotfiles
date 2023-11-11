35cb82e86b8e9b2392c31723c26acb68cd350453
Mason <masonwb@gmail.com>
2023-11-11 23:44:40 +0000

    updated path

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cp .bash_profile ~/.bash_profile
 cp .gitconfig ~/.gitconfig
 
 # install vscode configuration
-cp vscode/settings.json ~/.vscode-server/data/User
+cp vscode/settings.json ~/.local/share/code-server/User
 
 # configure git username
 git config --global user.email $CODER_USER_EMAIL
