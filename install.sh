dd05cc8e81465bb4bb880a0d9e29fc1b28a11164
Mason <masonwb@gmail.com>
2023-11-11 22:11:35 +0000

    install dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# install dotfiles
+cp .bash_profile ~/.bash_profile
+cp .gitconfig ~/.gitconfig
+
 # configure git username
 git config --global user.email $CODER_USER_EMAIL
 git config --global committer.email $CODER_USER_EMAIL
