7365d54564a10aa9c94f3ecfe15a76b8cb640a7f
Mason <masonwb@gmail.com>
2023-11-11 23:22:25 +0000

    don't need to install nvm anymore

--- install.sh
+++ install.sh
@@ -7,6 +7,3 @@ cp .gitconfig ~/.gitconfig
 # configure git username
 git config --global user.email $CODER_USER_EMAIL
 git config --global committer.email $CODER_USER_EMAIL
-
-# install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
