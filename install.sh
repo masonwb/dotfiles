880e477611cb55056500ae7085187b43d1a197aa
Mason <masonwb@gmail.com>
2023-11-11 23:48:07 +0000

    create base directories

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ cp .gitconfig ~/.gitconfig
 # install vscode configuration
 cp vscode/settings.json ~/.local/share/code-server/User
 
-# configure git username
+# configure git email
 git config --global user.email $CODER_USER_EMAIL
 git config --global committer.email $CODER_USER_EMAIL
+
+# create base directories
+mkdir -p ~/Development/{git,certs}
